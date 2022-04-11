d8422b28fc5cff78b475ee0453daf9fe7341f546
MayKashani <66264193+MayKashani@users.noreply.github.com>
2022-04-11 21:00:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/stops",jsonParser,(req,res)=>{
 });
 
 
-app.get("/stops",(req,res)=>{
+app.get("/getStops",(req,res)=>{
   ref.child("stops").child("parking").once("value",snapshot=>{
       if(snapshot.val() != null)
         res.status(200).send(snapshot.val())
