cb8465ec6a671945fa681b0eab30993fc2c21771
MayKashani <66264193+MayKashani@users.noreply.github.com>
2022-04-22 12:32:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -109,9 +109,7 @@ app.post("/route",jsonParser,(req,res)=>{
   let destination=req.body.destination
   let requestedTypes=req.body.requestedTypes
   calcRoute.initialRoute(origin,destination,requestedTypes)
-    .then(resRoute=>{
-      res.send(resRoute)
-      // res.status(200).send(resRoute)}
+    .then(resRoute=>res.status(200).send(resRoute))
 
 })
 
