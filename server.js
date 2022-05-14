a7ccf7b9ddee822c8a2b37bbd03d9b2179e89bf0
MayKashani <66264193+MayKashani@users.noreply.github.com>
2022-05-14 14:54:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const serviceAccount = require("./privateService.json");
 const axios = require ("axios");
 
 const jsonParser = bodyParser.json({ limit: '10mb', extended: true });
-const PORT =  5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
