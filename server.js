0119d16c6a6aee573b31b5a607c0063fd660966a
MayKashani <66264193+MayKashani@users.noreply.github.com>
2022-05-19 13:12:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const serviceAccount = require("./privateService.json");
 const axios = require ("axios");
 
 const jsonParser = bodyParser.json({ limit: '10mb', extended: true });
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000 || 3000
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -20,7 +20,7 @@ const ref = firebaseDB.ref()
 
 
 app.use(cors())
-
+app.use()
 app.post("/users",jsonParser,(req,res)=>{
   console.log(req.body)
   admin.auth().createUser({
