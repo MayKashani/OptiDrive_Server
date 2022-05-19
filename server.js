4726830d0e653294002419c7330088cc99572169
MayKashani <66264193+MayKashani@users.noreply.github.com>
2022-05-19 14:05:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ const ref = firebaseDB.ref()
 
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post("/users",jsonParser,(req,res)=>{
   console.log(req.body)
